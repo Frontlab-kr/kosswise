1c7cfeddc8f2901b2bdfd1b8e64117a1b658214b
eldestbro <123484134+Frontlab-kr@users.noreply.github.com>
2024-12-06 12:31:27 +0900

    2차 페이지 완료

--- assets/js/common.js
+++ assets/js/common.js
@@ -7,16 +7,27 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-  //검색폼
-  $('.kw-searchform-toggle').on('click', function () {
-    $(this).parents('.kw-searchform').toggleClass('kw-searchform--open');
-    if ($(this).parents('.kw-searchform').hasClass('kw-searchform--open')) {
-    } else {
-      $(this).parents('.kw-searchform').addClass('kw-searchform--tooltip');
-      setTimeout(() => {
-        $(this).parents('.kw-searchform').removeClass('kw-searchform--tooltip');
-      }, 3000);
-    }
+  // 검색폼
+  $('.kw-searchform').each(function () {
+    var $searchform = $(this);
+    var heightOpen = $searchform.outerHeight();
+    var heightFold = 68;
+
+    // 초기 높이 설정
+    $searchform.css({ height: heightOpen + 'px' });
+
+    $searchform.find('.kw-searchform-toggle').on('click', function () {
+      $searchform.toggleClass('kw-searchform--open');
+      if ($searchform.hasClass('kw-searchform--open')) {
+        $searchform.css({ height: heightOpen + 'px' });
+      } else {
+        $searchform.css({ height: heightFold + 'px' });
+        $searchform.addClass('kw-searchform--tooltip');
+        setTimeout(() => {
+          $searchform.removeClass('kw-searchform--tooltip');
+        }, 3000);
+      }
+    });
   });
 
   //align
@@ -31,6 +42,7 @@ $(document).ready(function () {
   $('.kw-align button').on('click', function (e) {
     // 버튼 클릭 시 부모에 클래스 토글
     e.stopPropagation(); // 이벤트 버블링 방지
+    $('.kw-align').removeClass('kw-align--open');
     $(this).parents('.kw-align').toggleClass('kw-align--open');
   });
 
@@ -44,15 +56,40 @@ $(document).ready(function () {
     function () {}
   );
 
+  $('.kw-tooltip__button').on('click', function () {
+    $('.kw-tooltip').removeClass('active');
+    $(this).parents('.kw-tooltip').toggleClass('active');
+  });
+  $(document).on('click', function (e) {
+    if (!$(e.target).closest('.kw-tooltip').length) {
+      $('.kw-tooltip').removeClass('active');
+    }
+  });
+
+  $(function () {
+    $('[data-bs-toggle="tooltip"]').tooltip();
+  });
+  $('[data-toggle=popover]').popover({
+    container: 'body', // body width
+  });
+
   //faq
   $('.kw-list-table-faq-title').on('click', function () {
-    $(this).parents('.table').find('tr').removeClass('active');
-    $('.kw-list-table-faq-contents__tr').removeClass('active');
     $(this)
       .parents('tr')
-      .addClass('active')
+      .toggleClass('active')
       .next('.kw-list-table-faq-contents__tr')
       .toggleClass('active');
-    return false;
+
+    //return false;
+  });
+
+  //tree
+  $('.kw-tree-collapse, .kw-tree-text label').on('click', function () {
+    $(this)
+      .parent('.kw-tree-text')
+      .parent('li')
+      .parent('ol')
+      .toggleClass('fold');
   });
 });
